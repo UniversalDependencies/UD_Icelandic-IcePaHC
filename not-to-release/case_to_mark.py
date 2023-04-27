2c94155fa3674e9ffa83d74e4477fd4c8ea530d4
thorunna <thar@hi.is>
2023-04-27 13:45:42 +0000

    updated rules on tag and deprel for conjunctions

--- not-to-release/case_to_mark.py
+++ not-to-release/case_to_mark.py
@@ -4,12 +4,13 @@ dev = "is_icepahc-ud-dev.conllu"
 test = "is_icepahc-ud-test.conllu"
 train = "is_icepahc-ud-train.conllu"
 
-corpus = pyconll.load_from_file(dev)
+input_file = train
+corpus = pyconll.load_from_file(input_file)
 
 for sentence in corpus:
     for token in sentence:
         if (
-            token.lemma in {"þegar", "ef", "nema", "þótt", "þó"}
+            token.lemma in {"þegar", "ef", "nema", "þótt", "þó", "meðan", "uns", "en"}
             and token.upos == "ADP"
             and token.deprel == "case"
         ):
@@ -18,7 +19,7 @@ for sentence in corpus:
             token.deprel = "mark"
             token.upos = "SCONJ"
 
-with open(dev, "w", encoding="utf-8") as f:
+with open(input_file, "w", encoding="utf-8") as f:
     for sentence in corpus:
         f.write("# sent_id = ")
         f.write(sentence.meta_value("sent_id"))
